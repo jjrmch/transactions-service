f98b7c82b14ecf0518eacb32d6f1163f63a697a1
Jaime Jose Romero Molina <jarromo@hotmail.es>
2026-08-14 17:42:26 +0200

    feat: resolver catalog-service por nombre via Eureka (sin URL fija)

--- src/main/java/com/biblioteca/transactions_service/client/CatalogClient.java
+++ src/main/java/com/biblioteca/transactions_service/client/CatalogClient.java
@@ -1,14 +1,14 @@
 package com.biblioteca.transactions_service.client;
 
+import com.biblioteca.transactions_service.dto.AjusteStockDto;
 import com.biblioteca.transactions_service.dto.LibroDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.biblioteca.transactions_service.dto.AjusteStockDto;
 
-@FeignClient(name = "catalog-service", url = "http://localhost:8081")
+@FeignClient(name = "catalog-service")
 public interface CatalogClient {
 
     @GetMapping("/libros/{id}")
